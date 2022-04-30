a7449cf812b8d5d4309e26175ea9324fa7f93d45
chengleileilei <775394475@qq.com>
2022-04-30 17:45:40 +0800

    完成团队风采数据构造

--- src/components/ListOfPapers.vue
+++ src/components/ListOfPapers.vue
@@ -6,12 +6,39 @@
         <div></div>
       </el-col>
     </el-row>
+
+    <el-row
+      class="centered lr-padding"
+      v-for="(papers, index) in paperList"
+      :key="index"
+    >
+      <div>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <p>{{ papers.time }}</p>
+      </div>
+
+      <el-row>
+        <el-col v-for="(paper, index) in papers.papers" :key="index">
+          <p>{{ paper.title }}</p>
+          <p>{{ paper.author }}</p>
+          <a :href="paper.link">[paper <img src="" alt="" /> ]</a>
+        </el-col>
+      </el-row>
+    </el-row>
   </div>
 </template>
 
 <script>
+import dataMock from "../../static/mockData.json";
+
 export default {
   name: "ListOfPapers",
+  data() {
+    return {
+      paperList: dataMock.paper_list,
+    };
+  },
 };
 </script>
 
